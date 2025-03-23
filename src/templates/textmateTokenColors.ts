bed424713be98b819df3a677a23fe17efb6f1724
ptrptrd <pptrd@protonmail.com>
2025-03-23 16:11:54 +0100

    add textmate token colors

--- src/templates/textmateTokenColors.ts
+++ src/templates/textmateTokenColors.ts
@@ -1,18 +1,18 @@
-import { base } from "../colors";
+import { base, fg, syntax, ui } from "../colors";
 
 const textmateTokenColors = [
   {
     "name": "Built-in constant",
     "scope": "constant.language",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Class name",
     "scope": "entity.name.class",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.class.hex()
     }
   },
   {
@@ -23,35 +23,35 @@ const textmateTokenColors = [
       "punctuation.definition.tag"
     ],
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.comment.hex()
     }
   },
   {
     "name": "Comment - javascript",
     "scope": ["comment.line.double-slash.js"],
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.comment.hex()
     }
   },
   {
     "name": "Enum",
     "scope": ["entity.name.type.enum", "entity.name.variable.enum-member"],
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.enum.hex()
     }
   },
   {
     "name": "Function argument",
     "scope": ["entity.name.variable.parameter", "variable.parameter.cpp"],
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.parameter.hex()
     }
   },
   {
     "name": "Field",
     "scope": "entity.name.variable.field",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.variable.hex()
     }
   },
   {
@@ -59,7 +59,7 @@ const textmateTokenColors = [
     "scope": "entity.name.function",
     "settings": {
       "fontStyle": "",
-      "foreground": base.red.hex()
+      "foreground": syntax.function.hex()
     }
   },
   {
@@ -67,14 +67,14 @@ const textmateTokenColors = [
     "scope": "entity.name.function.js",
     "settings": {
       "fontStyle": "",
-      "foreground": base.red.hex()
+      "foreground": syntax.function.hex()
     }
   },
   {
     "name": "Inherited class",
     "scope": "entity.other.inherited-class",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.class.hex()
     }
   },
   {
@@ -82,98 +82,98 @@ const textmateTokenColors = [
     "scope": "invalid",
     "settings": {
       "fontStyle": "",
-      "foreground": base.red.hex()
+      "foreground": ui.message.error.normal.foreground.hex()
     }
   },
   {
     "name": "Invalid deprecated",
     "scope": "invalid.deprecated",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": ui.message.warning.normal.foreground.hex()
     }
   },
   {
     "name": "JSON String",
     "scope": "meta.structure.dictionary.json string.quoted.double.json",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.string.hex()
     }
   },
   {
     "name": "Keyword",
     "scope": "keyword",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.keyword.hex()
     }
   },
   {
     "name": "Keyword",
     "scope": "keyword.other.class",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.keyword.hex()
     }
   },
   {
     "name": "Keyword control",
     "scope": "keyword.control",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.keyword.hex()
     }
   },
   {
     "name": "Keyword control from",
     "scope": "keyword.control.from",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.keyword.hex()
     }
   },
   {
     "name": "Keyword control import",
     "scope": "keyword.control.import",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.operator.hex()
     }
   },
   {
     "name": "Keyword other",
     "scope": "keyword.other",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Keyword other unit",
     "scope": "keyword.other.unit",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Keyword other using",
     "scope": "keyword.other.using",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Keyword type",
     "scope": "keyword.type",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.type.hex()
     }
   },
   {
     "name": "Library class/type",
     "scope": ["support.type", "support.class"],
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Library class/type - css",
     "scope": ["support.type.property-name.css"],
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
@@ -181,7 +181,7 @@ const textmateTokenColors = [
     "scope": "support.constant",
     "settings": {
       "fontStyle": "",
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
@@ -192,7 +192,7 @@ const textmateTokenColors = [
     ],
     "settings": {
       "fontStyle": "",
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
@@ -200,35 +200,35 @@ const textmateTokenColors = [
     "scope": "support.function",
     "settings": {
       "fontStyle": "",
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Meta",
     "scope": "meta",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.comment.hex()
     }
   },
   {
     "name": "Number",
     "scope": "constant.numeric",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.number.hex()
     }
   },
   {
     "name": "Property",
     "scope": "entity.name.variable.property",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.property.hex()
     }
   },
   {
     "name": "Punctuation",
     "scope": "punctuation",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
@@ -236,7 +236,7 @@ const textmateTokenColors = [
     "scope": "source",
     "settings": {
       "fontStyle": "",
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
@@ -244,175 +244,175 @@ const textmateTokenColors = [
     "scope": "storage",
     "settings": {
       "fontStyle": "",
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Storage modifier",
     "scope": "storage.modifier",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Storage type",
     "scope": "storage.type",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "String",
     "scope": "string",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.string.hex()
     }
   },
   {
     "name": "String - yaml",
     "scope": "string.unquoted.plain.out.yaml",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.string.hex()
     }
   },
   {
     "name": "Symbol",
     "scope": "meta.symbol",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Tag attribute",
     "scope": "entity.other.attribute-name",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Tag attribute",
     "scope": "entity.other.attribute-name.html",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Tag attribute - css",
     "scope": "entity.other.attribute-name.class.css",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Tag attribute id - css",
     "scope": "entity.other.attribute-name.id.css",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Tag name",
     "scope": "entity.name.tag",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Tag name - css",
     "scope": "entity.name.tag.css",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Tag name - YAML",
     "scope": "entity.name.tag",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": fg.hex()
     }
   },
   {
     "name": "Text",
     "scope": "text",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.string.hex()
     }
   },
   {
     "name": "This self",
     "scope": "variable.language",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.operator.hex()
     }
   },
   {
     "name": "Type",
     "scope": "entity.name.type",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.type.hex()
     }
   },
   {
     "name": "User-defined constant",
     "scope": ["constant.character", "constant.other"],
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.variable.hex()
     }
   },
   {
     "name": "Variable",
     "scope": "variable",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.variable.hex()
     }
   },
   {
     "name": "Variable local",
     "scope": "entity.name.variable.local",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.variable.hex()
     }
   },
   {
     "name": "Variable object property",
     "scope": "variable.object.property",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.parameter.hex()
     }
   },
   {
     "name": "Variable object property - javascript",
     "scope": "variable.other.property",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.parameter.hex()
     }
   },
   {
     "name": "Variable other constant",
     "scope": "variable.other.constant",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.variable.hex()
     }
   },
   {
     "name": "Variable other object",
     "scope": "variable.other.object",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.variable.hex()
     }
   },
   {
     "name": "Variable other readwrite",
     "scope": "variable.other.readwrite",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.variable.hex()
     }
   },
   {
     "name": "Variable parameter",
     "scope": "variable.parameter",
     "settings": {
-      "foreground": base.red.hex()
+      "foreground": syntax.parameter.hex()
     }
   }
 ]
