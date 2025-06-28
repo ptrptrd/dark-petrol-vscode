e100f3478a1c05ff4bb410caf5f8c4b0c844091c
ptrptrd <pptrd@protonmail.com>
2025-06-28 19:11:11 +0200

    add mkdir first

--- src/build.ts
+++ src/build.ts
@@ -4,4 +4,5 @@ import template from './template'
 
 
 const theme = JSON.stringify(template(), null, '\t')
+fs.mkdirSync(path.dirname("themes"), { recursive: true })
 fs.writeFileSync(path.join(process.cwd(), `themes/dark-petrol-dev.json`), theme)
